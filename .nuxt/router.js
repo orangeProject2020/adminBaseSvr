cafc1aea0196e4331da9413df52c981987a3b575
lucong <332553882@qq.com>
2019-12-05 15:15:08 +0800

    update

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -6,6 +6,8 @@ import scrollBehavior from './router.scrollBehavior.js'
 const _024885ab = () => interopDefault(import('../pages/app.vue' /* webpackChunkName: "pages/app" */))
 const _94874918 = () => interopDefault(import('../pages/user/index.vue' /* webpackChunkName: "pages/user/index" */))
 const _59900028 = () => interopDefault(import('../pages/auth/login.vue' /* webpackChunkName: "pages/auth/login" */))
+const _1ef1765c = () => interopDefault(import('../pages/msg/list.vue' /* webpackChunkName: "pages/msg/list" */))
+const _6d5671e8 = () => interopDefault(import('../pages/msg/notice.vue' /* webpackChunkName: "pages/msg/notice" */))
 const _47aadf5c = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 Vue.use(Router)
@@ -29,6 +31,14 @@ export const routerOptions = {
     path: "/auth/login",
     component: _59900028,
     name: "auth-login"
+  }, {
+    path: "/msg/list",
+    component: _1ef1765c,
+    name: "msg-list"
+  }, {
+    path: "/msg/notice",
+    component: _6d5671e8,
+    name: "msg-notice"
   }, {
     path: "/",
     component: _47aadf5c,

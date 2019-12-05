27972eb6f85d88fc886f76e745a2b356941e9baf
lucong <332553882@qq.com>
2019-12-05 10:09:37 +0800

    update

--- pages/msg/list.vue
+++ pages/msg/list.vue
@@ -3,14 +3,39 @@
     <sub-nav-msg></sub-nav-msg>
     <el-table :data="listData.list">
       <el-table-column label="ID" prop="id"></el-table-column>
-
-      <el-table-column align="right">
-        <template slot="header" slot-scope="scope">
-          <el-input v-model="listData.search" size="mini" placeholder="输入关键字搜索" />
-        </template>
+      <el-table-column label="类型">
         <template slot-scope="scope">
-          <el-button size="mini">查看</el-button>
-          <el-button size="mini" type="danger">禁用</el-button>
+          <span v-if="scope.row.type == 0">用户消息</span>
+          <span v-if="scope.row.type == 1">公告消息</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="信息">
+        <template slot-scope="scope">
+          <span v-if="scope.row.type == 0">{{scope.row.info}}</span>
+          <template v-if="scope.row.type == 1 && scope.row.notice_id > 0">
+            <span
+              v-for="notice in noticeList"
+              v-if="scope.row.notice_id == notice.id"
+              :key="notice.id"
+            >{{notice.id}}.{{notice.title}}</span>
+          </template>
+        </template>
+      </el-table-column>
+      <el-table-column label="发送时间">
+        <template slot-scope="scope">
+          {{
+          dateFormat(scope.row.create_time)
+          }}
+        </template>
+      </el-table-column>
+      <el-table-column label="用户信息">
+        <template slot-scope="scope">
+          <div v-if=" scope.row.user_id > 0">
+            <span
+              v-for="user in userList"
+              v-if="scope.row.user_id == user.id"
+            >{{user.id}}.{{user.username || user.mobile}}</span>
+          </div>
         </template>
       </el-table-column>
     </el-table>
@@ -29,6 +54,7 @@
 
 <script>
 import apis from "@/assets/js/apis";
+import utils from "@/assets/js/utils";
 import SubNavMsg from "@/components/SubNavMsg.vue";
 export default {
   components: {
@@ -44,13 +70,16 @@ export default {
       listData: {
         list: [],
         page: 1,
-        limit: 1,
+        limit: 10,
         count: 0,
         search: ""
-      }
+      },
+      userList: "",
+      noticeList: ""
     };
   },
   methods: {
+    ...utils,
     async getListData() {
       let data = {};
       data.page = this.listData.page;
@@ -58,7 +87,7 @@ export default {
       data.search = this.listData.search;
 
       try {
-        let dataRet = await apis.getUserListData(data);
+        let dataRet = await apis.getMessageListData(data);
         console.log("/getListData dataRet:", dataRet);
         if (dataRet.code === 0) {
           this.listData.count = dataRet.data.count;
@@ -72,14 +101,51 @@ export default {
         this.$message.error(err.message || "获取列表数据失败");
       }
     },
+    getRefreshData() {
+      this.listData.list = [];
+      this.listData.count = 0;
+      this.getListData().then(() => {
+        this.getUserList();
+        this.getNoticeList();
+      });
+    },
     pageChange(page) {
       this.listData.list = [];
       this.listData.page = page;
       this.getListData();
+    },
+    async getUserList() {
+      let userIds = [];
+      this.userList = [];
+      this.listData.list.forEach(item => {
+        userIds.push(item.user_id);
+      });
+
+      let ret = await apis.getUserListData({ ids: userIds, limit: 0 });
+      console.log("/getUserList ret:", ret);
+      if (ret.code === 0) {
+        this.userList = ret.data.rows;
+      }
+    },
+    async getNoticeList() {
+      let noticeIds = [];
+      this.noticeList = [];
+      this.listData.list.forEach(item => {
+        noticeIds.push(item.user_id);
+      });
+
+      let ret = await apis.getNoticeListData({ ids: noticeIds, limit: 0 });
+      console.log("/getNoticeList ret:", ret);
+      if (ret.code === 0) {
+        this.noticeList = ret.data.rows;
+      }
     }
   },
   created() {
-    this.getListData();
+    this.getListData().then(() => {
+      this.getUserList();
+      this.getNoticeList();
+    });
   }
 };
 </script>
